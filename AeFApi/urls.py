a5af774080390572ef780a940bbcf8fe894c49d7
Mohsin <mohseny@mississaugajamaat.com>
2019-02-16 16:54:04 +0000

    updated

--- AeFApi/urls.py
+++ AeFApi/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls',
                               namespace='rest_framework')),
-    path('admin/', admin.site.urls)
+    path('', admin.site.urls),
 ]
 
 admin.site.site_header = "Mississauga Jamaat Admin"
